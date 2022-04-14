502576d84fa3734e01534e5fcc41d639765ab93e
Sarertnoc <g.sarertnoc12@gmmail.com>
2022-04-14 15:53:38 -0500

    agregando firebase

---
+++ src/components/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCKKJ1WtWNBxg-QPVmD10XXX2AjMFb8c-s",
+  authDomain: "fly-keywords.firebaseapp.com",
+  databaseURL: "https://fly-keywords-default-rtdb.firebaseio.com",
+  projectId: "fly-keywords",
+  storageBucket: "fly-keywords.appspot.com",
+  messagingSenderId: "621545019241",
+  appId: "1:621545019241:web:3a6017946676c391160ff8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
