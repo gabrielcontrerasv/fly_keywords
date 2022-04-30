cab6f76e64bf8de91d31b1fe9b87d06ce6310d62
jrcerquerainnova <junior.cerquera@innova.edu.co>
2022-04-29 19:00:16 -0500

    ajustando importacion de asset

--- src/components/logo/index.jsx
+++ src/components/logo/index.jsx
@@ -1,21 +1,19 @@
-import react from 'react';
-import reactDom from 'react-dom';
-
+import react from "react";
+import reactDom from "react-dom";
+import logofac from "../../assets/imgs/logofac.png";
 
 function Logo() {
-    //const [count, setCount] = useState(0)
-  
-    return (
-      <div className='row d-flex d-flex justify-content-center'>
-        <div className='mt-5 col-4 d-flex d-flex justify-content-center'>
-          <img src='./src/assets/imgs/logofac.png' className='mt-5'/>
+  //const [count, setCount] = useState(0)
+
+  return (
+    <div className="row d-flex d-flex justify-content-center">
+      <div className="mt-5 col-4 d-flex d-flex justify-content-center">
+        <img src={logofac} className="mt-5" />
       </div>
-      </div>
-    );
-  }
-  
-  export default Logo;
+    </div>
+  );
+}
 
+export default Logo;
 
-
-  //<img src='./assets/imgs/logfac.png'/>
+//<img src='./assets/imgs/logfac.png'/>
